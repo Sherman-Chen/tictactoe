826a7967e56fbfb101f4c01642c971947c6127e0
Sherman <shc410@gmail.com>
2016-12-05 15:20:00 -0800

    toggleX for toggling a board position to X

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 console.log('hello world!');
 
 var Board = function() {
-  this.board = [[0,0,0],[0,0,0],[0,0,0]];
+  this.board = [['-','-','-'],['-','-','-'],['-','-','-']];
 } // end Board
 
 Board.prototype = {
@@ -11,15 +11,17 @@ Board.prototype = {
     // this prints out the current status of the board
     console.log(this.board);
   }, // end print
-  toggle: function(row, column) {
+  toggleX: function(row, column) {
     // toggle should -> 
       // find the row and column within the respective row you wish to toggle
       // toggle the row as per your respective user (X or O);
         // before toggling it needs to check if the row is togglable (i.e. you cannot overwrite something that is already toggled)
       // upon successful toggle, print the board and then it will need to run checkWinner to see if the toggling created a game winning condition  
-    for (var i = 0; i < this.board.length; i++) {
-      console.log('toggling');
+    if (this.board[row][column] === '-') {
+      this.board[row][column] = 'X';
     }
+    print();
+    console.log('Next Players Turn!');
   }, //end toggle
   checkWinner: function() {
     // should check to see if toggling a piece enabled a win condition
@@ -27,11 +29,10 @@ Board.prototype = {
   }, // end checkWinner
   clear: function() {
     // resets board state to default
-    this.board = [[0,0,0],[0,0,0],[0,0,0]];
+    this.board = [['-','-','-'],['-','-','-'],['-','-','-']];
   }
 } // end Board.prototype
 
 var board = new Board();
 
 // board.print();
-board.toggle();
