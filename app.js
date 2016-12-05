509fa2bbffcb23247dd6dd62cc13114b839d02a0
Sherman <shc410@gmail.com>
2016-12-05 15:23:44 -0800

    add toggleO method

--- app.js
+++ app.js
@@ -19,10 +19,27 @@ Board.prototype = {
       // upon successful toggle, print the board and then it will need to run checkWinner to see if the toggling created a game winning condition  
     if (this.board[row][column] === '-') {
       this.board[row][column] = 'X';
+      console.log('Next Players Turn!');
+    } else {
+      console.log('position is already an O!');
     }
-    print();
+    this.print();
+  }, //end toggleX
+  toggleO: function(row, column) {
+    // toggle should -> 
+      // find the row and column within the respective row you wish to toggle
+      // toggle the row as per your respective user (X or O);
+        // before toggling it needs to check if the row is togglable (i.e. you cannot overwrite something that is already toggled)
+      // upon successful toggle, print the board and then it will need to run checkWinner to see if the toggling created a game winning condition  
+    if (this.board[row][column] === '-') {
+      this.board[row][column] = 'O';
+      console.log('Next Players Turn!');      
+    } else {
+      console.log('position is already an X');
+    }
+    this.print();
     console.log('Next Players Turn!');
-  }, //end toggle
+  }, //end toggleO
   checkWinner: function() {
     // should check to see if toggling a piece enabled a win condition
 
@@ -35,4 +52,10 @@ Board.prototype = {
 
 var board = new Board();
 
-// board.print();
+board.toggleX(0,0);
+board.toggleO(0,0);
+board.toggleO(0,1);
+
+
+
+
