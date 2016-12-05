6fdedd8436f0958917ecbba5030a626c9f372204
Sherman <shc410@gmail.com>
2016-12-05 14:56:03 -0800

    create Board prototype to add methods to

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ var Board = function() {
   this.board = [[0,0,0],[0,0,0],[0,0,0]];
 }
 
+Board.prototype = {
+  print: function() {
+    console.log('hey');
+  }
+}
+
 var board = new Board();
 
-console.log(JSON.stringify(board));
+board.print();
